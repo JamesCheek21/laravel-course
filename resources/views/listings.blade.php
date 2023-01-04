15726c242ca088da628398acd9f363ed411a72ed
JamesCheek21 <james.cheek@twine.net>
2023-01-04 20:48:47 +0000

    Added layout html, components with classes, slots, and components, and partials

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,11 +1,17 @@
-<h1>{{ $heading }}</h1>
-@unless(count($listings) == 0)
-    @foreach ($listings as $listing)
-        <h2>
-            <a href="/listings/{{ $listing['id'] }}"> {{ $listing['title'] }}</a>
-        </h2>
-        <p>{{ $listing['description'] }}</p>
-    @endforeach
-@else
-    <p>No listings found</p>
-@endunless
+@extends('layout')
+
+
+@section('content')
+    @include('partials./_hero')
+    @include('partials./_search')
+
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+        @unless(count($listings) == 0)
+            @foreach ($listings as $listing)
+                <x-listing-card :listing="$listing" />
+            @endforeach
+        @else
+            <p>No listings found</p>
+        @endunless
+    </div>
+@endsection
