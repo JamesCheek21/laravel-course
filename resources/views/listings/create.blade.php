9f58d127782a77bb695628df3e6f11536bd6cd17
JamesCheek21 <james.cheek@twine.net>
2023-01-05 13:35:55 +0000

    Recolour

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -91,7 +91,7 @@
             </div>
 
             <div class="mb-6">
-                <button class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
+                <button class="bg-purple-700 text-white rounded py-2 px-4 hover:bg-black">
                     Create Gig
                 </button>
 
